2513ee659299e3db800f6df65bdb06289b4e2b73
Jeff Minucci <jminucci@uga.edu>
2018-09-20 10:14:27 -0400

    fix encoding of csv-jsons for mongodb

--- app/nta_task.py
+++ app/nta_task.py
@@ -30,7 +30,6 @@ class NtaRun:
     def execute(self):
         print(self.entact)
         # 1: drop duplicates
-        self.create_mongo_post()
         self.drop_duplicates()
         if self.verbose:
             print("Dropped duplicates.")
@@ -58,8 +57,8 @@ class NtaRun:
 
     def drop_duplicates(self):
         self.dfs = [duplicates(df, index) for index, df in enumerate(self.dfs)]
-        self.save_df_to_mongo('input_no_duplicates_pos', self.dfs[0])
-        self.save_df_to_mongo('input_no_duplicates_neg', self.dfs[1])
+        #self.mongo_save(self.dfs[0], 'input_no_duplicates_pos')
+        #self.mongo_save( self.dfs[1], 'input_no_duplicates_neg')
         return
 
     def calc_statistics(self):
@@ -70,6 +69,8 @@ class NtaRun:
         print(self.dfs[0].shape)
         #self.save_df_to_mongo('stats_pos', self.dfs[0])
         #self.save_df_to_mongo('stats_neg', self.dfs[1])
+        self.mongo_save(self.dfs[0], 'stats_pos')
+        self.mongo_save(self.dfs[1], 'stats_neg')
         return
 
 
@@ -81,8 +82,8 @@ class NtaRun:
             print("Tracer file found, checking tracers.")
         ppm = self.mass_accuracy_units_tr == 'ppm'
         self.tracer_dfs_out = [check_feature_tracers(df, self.tracer_df, self.mass_accuracy_tr, self.rt_accuracy_tr, ppm) for index, df in enumerate(self.dfs)]
-        self.save_df_to_mongo('tracers_pos', self.tracer_dfs_out[0])
-        self.save_df_to_mongo('tracers_neg', self.tracer_dfs_out[1])
+        self.mongo_save(self.tracer_dfs_out[0], 'tracers_pos')
+        self.mongo_save(self.tracer_dfs_out[1], 'tracers_neg')
         return
 
     def clean_features(self):
@@ -90,14 +91,17 @@ class NtaRun:
         pass
 
 
-    def create_mongo_post(self):
+    def mongo_save(self, file, step=""):
+        to_save = json.loads(file.to_json(orient='index'))
         posts = self.mongo.posts
         time_stamp = datetime.utcnow()
-        data = {'_id': self.jobid, 'date': time_stamp}
+        id = self.jobid + "_" + step
+        data = {'_id': id, 'date': time_stamp, 'data': to_save}
         posts.insert_one(data)
 
-    def save_df_to_mongo(self, label, df): #TODO: save each csv as a new item, save raw csv as binary not json
-        posts = self.mongo.posts
-        time_stamp = datetime.utcnow()
-        data = df.to_json(orient='index')
-        posts.update_one({'_id': self.jobid}, {'$set': {label: data}})
+
+    # def save_df_to_mongo(self, label, df): #TODO: save each csv as a new item, save raw csv as binary not json
+    #     posts = self.mongo.posts
+    #     time_stamp = datetime.utcnow()
+    #     data = df.to_json(orient='index')
+    #     posts.update_one({'_id': self.jobid}, {'$set': {label: data}})
