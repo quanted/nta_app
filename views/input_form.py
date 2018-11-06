1497d76161c7f35afa14577bbb8b3f718dbb6c31
Tom Purucker <purucker.tom@gmail.com>
2018-11-06 11:59:44 -0500

    removing pram dependency

--- views/input_form.py
+++ views/input_form.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, FileExtensionValidator
 from django.utils.safestring import mark_safe
 from django.forms.widgets import NumberInput
 
-from pram_app.models.forms import validation
+from nta_app.models.forms import validation
 
 
 class RangeInput(NumberInput):
