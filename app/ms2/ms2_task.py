4630e088d7e861f25def0b453b2414c059f7f24d
JeffreyMinucci <jminucci2@gmail.com>
2019-11-20 11:58:56 -0500

    add skeleton for ms2 app tasks

---
+++ app/ms2/ms2_task.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import os
+import csv
+import time
+import logging
+import traceback
+import shutil
+import json
+from datetime import datetime
+from dask.distributed import Client, LocalCluster, fire_and_forget
+
+from .utilities import connect_to_mongoDB, connect_to_mongo_gridfs
+
+NO_DASK = False  # set this to True to run locally without dask (for debug purposes)
+
+logger = logging.getLogger("nta_app.ms2")
+logger.setLevel(logging.INFO)
+
+def run_ms2_dask(parameters, input_dfs, jobid = "00000000", verbose = True):
+    in_docker = os.environ.get("IN_DOCKER") != "False"
+    mongo_address = os.environ.get('MONGO_SERVER')
+    if NO_DASK:
+        run_ms2(parameters, input_dfs, mongo_address, jobid, verbose, in_docker = in_docker)
+        return
+    if not in_docker:
+        logger.info("Running in local development mode.")
+        logger.info("Detected OS is {}".format(os.environ.get("SYSTEM_NAME")))
+        local_cluster = LocalCluster(processes=False, ip='127.0.0.1')
+        dask_client = Client(local_cluster)
+    else:
+        dask_scheduler = os.environ.get("DASK_SCHEDULER")
+        logger.info("Running in docker environment. Dask Scheduler: {}".format(dask_scheduler))
+        dask_client = Client(dask_scheduler, processes=False)
+    dask_input_dfs = dask_client.scatter(input_dfs)
+    logger.info("Submitting Nta ms2 Dask task")
+    task = dask_client.submit(run_ms2, parameters, dask_input_dfs, tracer_df, mongo_address, jobid, verbose,
+                              in_docker = in_docker)
+    fire_and_forget(task)
+
+
+def run_ms2(parameters, input_dfs,  mongo_address = None, jobid = "00000000", verbose = True,
+            in_docker = True):
+    ms2_run = MS2Run(parameters, input_dfs, mongo_address, jobid, verbose, in_docker = in_docker)
+    try:
+        ms2_run.execute()
+    except Exception as e:
+        trace = traceback.format_exc()
+        logger.info(trace)
+        fail_step = ms2_run.get_step()
+        ms2_run.set_status("Failed on step: " + fail_step)
+        error = repr(e)
+        ms2_run.set_except_message(error)
+        raise e
+    return True
+
+
+FILENAMES = {'duplicates': ['duplicates_dropped_pos', 'duplicates_dropped_neg'],
+             'stats': ['stats_pos', 'stats_neg'],
+             'tracers': ['tracers_pos', 'tracers_neg'],
+             'cleaned': ['cleaned_pos', 'cleaned_neg'],
+             'flags': ['flags_pos', 'flags_neg'],
+             'combined': 'combined',
+             'mpp_ready': ['for_stats_full', 'for_stats_reduced'],
+             'dashboard': 'dashboard_search',
+             'toxpi': ['final_output_full', 'final_output_reduced']
+             }
+
+
+class MS2Run:
+    
+    def __init__(self, parameters=None, input_dfs=None, mongo_address = None, jobid = "00000000",
+                 verbose = True, in_docker = True):
+        self.jobid = jobid
+        self.verbose = verbose
+        self.in_docker = in_docker
+        self.mongo_address = mongo_address
+        self.mongo = connect_to_mongoDB(self.mongo_address)
+        self.gridfs = connect_to_mongo_gridfs(self.mongo_address)
+        self.step = "Started"  # tracks the current step (for fail messages)
+        pass
+
+    def execute(self):
+        pass
+
+    def set_status(self, status, create = False):
+        posts = self.mongo.posts
+        time_stamp = datetime.utcnow()
+        post_id = self.jobid + "_" + "status"
+        if create:
+            posts.update_one({'_id': post_id},{'$set': {'_id': post_id,
+                                                   'date': time_stamp,
+                                                   'status': status,
+                                                   'error_info': ''}},
+                             upsert=True)
+        else:
+            posts.update_one({'_id': post_id}, {'$set': {'_id': post_id,
+                                                    'status': status}},
+                             upsert=True)
+
+    def set_except_message(self, e):
+        posts = self.mongo.posts
+        time_stamp = datetime.utcnow()
+        post_id = self.jobid + "_" + "status"
+        posts.update_one({'_id': post_id},{'$set': {'_id': post_id,
+                                              'date': time_stamp,
+                                              'error_info': e}},
+                         upsert=True)
+
+    def get_step(self):
+        return self.step
+
+    def mongo_save(self, file, step=""):
+        to_save = file.to_json(orient='split')
+        id = self.jobid + "_" + step
+        self.gridfs.put(to_save, _id=id, encoding='utf-8', project_name = self.project_name)
