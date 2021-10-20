3266c5a689dfe0ae8a62de80bd81d12f1f9f4588
Deron Smith <deron.brock@gmail.com>
2021-10-20 08:56:59 -0500

    update settings

--- settings.py
+++ settings.py
@@ -113,7 +113,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "collected_static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "collected_static/nta")
 
 # Define ENVIRONMENTAL VARIABLES
 os.environ.update({
