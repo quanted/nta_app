c1b68316028707a1a7450db64e8bf3ed163c853c
EC2 Default User <ec2-user@ip-10-1-18-159.ec2.internal>
2021-12-14 20:44:25 +0000

    Moved log entry for debugging

--- app/ms1/nta_task.py
+++ app/ms1/nta_task.py
@@ -328,9 +328,9 @@ class NtaRun:
         #    self.mongo_save(self.search_results, FILENAMES['dashboard'])
         
     def search_hcd(self):
+        logging.info(f'Querying the HCD with DTXSID identifiers from: {self.search_results}')
         if len(self.search_results) > 0:
             dtxsid_list = self.search_results['DTXSID']
-            logging.info('Querying the HCD with {} DTXSID identifiers'.format(len(self.search_results)))
             hcd_results = api_search_hcd(dtxsid_list)
             self.search_results = self.search_results.merge(hcd_results, how = 'left', on = 'DTXSID')
     
