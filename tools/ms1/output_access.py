423c8f794adaaf5e4005edf40be943438ab833ae
Tommy E. Cathey <tecathey@gmail.com>
2023-05-08 14:14:32 -0400

    implements NTAW-110 (Tracer plots not being returned)

--- tools/ms1/output_access.py
+++ tools/ms1/output_access.py
@@ -1,16 +1,17 @@
 import pandas as pd
-import json
 import os
-import openpyxl
 import time
 from datetime import datetime
-from io import StringIO, BytesIO
-from PIL import Image
+from io import BytesIO
 from zipfile import ZipFile, ZIP_DEFLATED
 from django.http import HttpResponse, JsonResponse
 from ...app.ms1.utilities import connect_to_mongoDB, connect_to_mongo_gridfs
 from pymongo.errors import OperationFailure
 from gridfs.errors import NoFile
+
+# used for parallelization
+# import concurrent.futures
+# import collections
 
 #os.environ['IN_DOCKER'] = "False" #for local dev - also see similar switch in app/nta_task.py
 
@@ -50,80 +51,123 @@ class OutputServer:
         response_data = {'start_time': time, 'status': status, 'error_info': except_text}
         return JsonResponse(response_data)
 
-    def final_result(self):
-        #
-        #This code and the output code need to be optimized to improve compute time to generete xlsx file and transfer time of file to client
-        #
-        initial = time.perf_counter()
-        in_memory_buffer = BytesIO()
+
+    #-----------------------------------------------------------------
+    #
+    # BEGIN PARALLELIZATION OF XLSX GENERATION
+    #
+    #-----------------------------------------------------------------
+
+    # def generate_excel_sheet(self, name):
+    #     try:
+    #         print(f'Constructing {name} file')
+    #         start = time.perf_counter()
+    #         id = self.jobid + "_" + name
+    #         db_record = self.gridfs.get(id)
+    #         json_string = db_record.read().decode('utf-8')
+    #         df = pd.read_json(json_string, orient='split')
+    #         return name, df
+    #     except Exception as e:
+    #         print(e)
+
+    # def generate_excel(self):
+    #     file_names = self.gridfs.get(f'{self.jobid}_file_names').read().decode('utf-8').split("&&")
+    #     print(f'file_names: {file_names}')
+
+    #     in_memory_buffer = BytesIO()
+    #     sheet_dict = collections.OrderedDict()
+
+    #     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    #         futures = [executor.submit(self.generate_excel_sheet, name) for name in file_names if 'final_' not in name]
+
+    #         for future in concurrent.futures.as_completed(futures):
+    #             try:
+    #                 name, df = future.result()
+    #                 sheet_dict[name] = df
+    #             except Exception as e:
+    #                 print(e)
+
+    #     with pd.ExcelWriter(in_memory_buffer, engine='openpyxl') as writer:
+    #         # for name, df in sheet_dict.items():
+    #         #     print(f'name of sheet {name}')
+    #         #     df.to_excel(writer, sheet_name=name, index=False)
+    #         for name in file_names:
+    #             if 'final_' not in name:
+    #                 df = sheet_dict.get(name)
+    #                 if df is not None:
+    #                     print(f'name of sheet {name}')
+    #                     df.to_excel(writer, sheet_name=name, index=False)
+
+    #     return in_memory_buffer.getvalue()
+
+    #-----------------------------------------------------------------
+    #
+    # END PARALLELIZATION OF XLSX GENERATION
+    #
+    #-----------------------------------------------------------------
+
+
+    def construct_excel_sheet(self, writer, name):
+        try:
+            print(f'Constructing {name} file')
+            start = time.perf_counter()
+            id = self.jobid + "_" + name
+            db_record = self.gridfs.get(id)
+            json_string = db_record.read().decode('utf-8')
+            df = pd.read_json(json_string, orient='split')
+            df.to_excel(writer, sheet_name=name, index=False)
+            stop = time.perf_counter()
+            print(f'Time to construct {name}: {stop - start}')
+        except Exception as e:
+            print(e)
+
+    def generate_excel(self):
         file_names = self.gridfs.get(f'{self.jobid}_file_names').read().decode('utf-8').split("&&")
+        print(f'file_names: {file_names}')
+        in_memory_buffer = BytesIO()
         with pd.ExcelWriter(in_memory_buffer, engine='openpyxl') as writer:
             for name in file_names:
-                if 'final_' in name:          #Added to skip including the 'full_ouput' data in the final results xlsx. Doubles file size and greatly increases
-                    continue                        #compute time to preapre the file and transfer time to get file to client. Need better solution down road (compression)
-                try:        
-                    print(f'Constructing {name} file')                                                
-                    start = time.perf_counter()
-                    id = self.jobid + "_" + name
-                    db_record = self.gridfs.get(id)
-                    json_string = db_record.read().decode('utf-8')
-                    df = pd.read_json(json_string, orient='split')
-                    df.to_excel(writer, sheet_name=name, index = False)
-                    stop = time.perf_counter()
-                    print(f'Time to construct {name}: {stop - start}')
-                except Exception as e:
-                    print(e)
+                if 'final_' in name:
                     continue
-        response = HttpResponse(in_memory_buffer.getvalue(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = 'attachment; filename=summary.xlsx'
-        end = time.perf_counter()
-        print(f'Time to get xlsx: {end - initial}')
-        return response
+                self.construct_excel_sheet(writer, name)
+        return in_memory_buffer.getvalue()
+    
 
-    def all_files(self):
-        in_memory_zip = BytesIO()
-        file_names = self.gridfs.get(f'{self.jobid}_file_names').read().decode('utf-8').split("&&")
+
+    def add_tracer_plots_to_zip(self, zipf,jobid):
         tracer_plots = self.gridfs.get(f'{self.jobid}_tracer_files').read().decode('utf-8').split("&&")
-        
+        for name in tracer_plots:
+            try:
+                tracer_id = jobid + "_" + name
+                db_record = self.gridfs.get(tracer_id)
+                buffer = db_record.read()
+                project_name = db_record.project_name
+                if project_name:
+                    filename = project_name.replace(" ", "_") + '_' + name + '.png'
+                else:
+                    filename = tracer_id + '.png'
+                zipf.writestr(filename, buffer)
+            except (OperationFailure, TypeError, NoFile) as e:
+                break
+
+    def final_result(self):
+        in_memory_zip = BytesIO()
+            
         with ZipFile(in_memory_zip, 'w', ZIP_DEFLATED) as zipf:
-            for name in file_names:
-                try:
-                    record_id = self.jobid + "_" + name
-                    db_record = self.gridfs.get(record_id)
-                    json_string = db_record.read().decode('utf-8')
-                    df = pd.read_json(json_string, orient='split')
-                    project_name = db_record.project_name
-                    if project_name:
-                        filename = project_name.replace(" ", "_") + '_' + name + '.csv'
-                    else:
-                        filename = record_id + '.csv'
-                    csv_string = df.to_csv(index = False)
-                    zipf.writestr(filename, csv_string)
+            excel_data = self.generate_excel()
+            zipf.writestr('summary.xlsx', excel_data)
 
-                except (OperationFailure, TypeError, NoFile) as e:
-                    break
-
-            for name in tracer_plots:
-                try:
-                    tracer_id = self.jobid + "_" + name
-                    db_record = self.gridfs.get(tracer_id)
-                    buffer = db_record.read()
-                    project_name = db_record.project_name
-                    if project_name:
-                        filename = project_name.replace(" ", "_") + '_' + name + '.png'
-                    else:
-                        filename = tracer_id + '.png'
-                    csv_string = df.to_csv(index=False)
-                    zipf.writestr(filename, buffer)
-                except (OperationFailure, TypeError, NoFile) as e:
-                    break
-
+            self.add_tracer_plots_to_zip(zipf, self.jobid)
 
         zip_filename = 'nta_results_' + self.jobid + '.zip'
         response = HttpResponse(in_memory_zip.getvalue(),content_type='application/zip')
         response['Content-Disposition'] = 'attachment; filename=' + zip_filename
         response['Content-length'] = in_memory_zip.tell()
         return response
+
+
+
+
 
 
 
