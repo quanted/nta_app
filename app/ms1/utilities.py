90e9d8818ce99f7c0859233f4f043931acb0bc70
tmferland <tferland13@gmail.com>
2023-11-06 21:44:02 +0000

    changed 'reduced_file()' in utilities.py to reflect swap from median to mean

--- app/ms1/utilities.py
+++ app/ms1/utilities.py
@@ -46,14 +46,14 @@ def remove_columns(df_in, list_of_columns2remove):
 def reduced_file(df_in):
     df = df_in.copy()
     headers = parse_headers(df, 0)
-    keeps_str = ['MB_', 'blank', 'blanks', 'BLANK', 'Blank', 'Median', 'Sub']
+    keeps_str = ['MB_', 'blank', 'blanks', 'BLANK', 'Blank', 'Mean', 'Sub']
     to_drop = [item for sublist in headers for item in sublist if
                         (len(sublist) > 1) & (not any(x in item for x in keeps_str))]
     to_drop.extend(df.columns[(df.columns.str.contains(pat ='CV_|N_Abun_|Mean_|STD_')==True)].tolist())
-    to_drop.extend(df.columns[(df.columns.str.contains(pat ='Median_') == True) &
+    to_drop.extend(df.columns[(df.columns.str.contains(pat ='Mean_') == True) &
                               (df.columns.str.contains(pat ='MB|blank|blanks|BLANK|Blank|Sub')==False)].tolist())
-    if 'Median_ALLMB' in df.columns.values.tolist():
-        to_drop.extend(['Median_ALLMB'])
+    if 'Mean_ALLMB' in df.columns.values.tolist():
+        to_drop.extend(['Mean_ALLMB'])
     df.drop(to_drop, axis=1, inplace=True)
     return df
 
