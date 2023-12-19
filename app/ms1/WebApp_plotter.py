701d7d56e751a7a52461333f466b0da7e17c0964
Alex Chao <chao.alex@epa.gov>
2023-12-19 18:45:43 +0000

    Update import statement in utilities.py

--- app/ms1/WebApp_plotter.py
+++ app/ms1/WebApp_plotter.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import math
-from .functions_Universal_v3 import parse_headers
+#from .functions_Universal_v3 import parse_headers
+from .task_functions import parse_headers
 import io
 
 class WebApp_plotter:
