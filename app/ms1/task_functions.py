f8d465bc6fe4d0618c342c21f18490d2977327f5
tmferland <tferland13@gmail.com>
2024-04-19 20:53:33 +0000

    appending .fillna(False) to mask generation in occ_drop_df()

--- app/ms1/task_functions.py
+++ app/ms1/task_functions.py
@@ -1323,21 +1323,21 @@ def occ_drop_df(df, docs, df_flagged, Mean_Samples):
     # Create mask of occurrences dropped for replicate flag
     rep_fails = pd.concat(
         [docs[mean].str.contains("R") for mean in Mean_Samples], axis=1
-    )
+    ).fillna(False)
     # Mask df and df_flagged
     df[Mean_Samples] = df[Mean_Samples].mask(rep_fails)
     df_flagged[Mean_Samples] = df_flagged[Mean_Samples].mask(rep_fails)
     # Create mask of occurrences dropped for replicate flag
     non_detects = pd.concat(
         [docs[mean].str.contains("ND") for mean in Mean_Samples], axis=1
-    )
+    ).fillna(False)
     # Mask df and df_flagged
     df[Mean_Samples] = df[Mean_Samples].mask(non_detects)
     df_flagged[Mean_Samples] = df_flagged[Mean_Samples].mask(non_detects)
     # Create mask of occurrences dropped for replicate flag
     cv_fails = pd.concat(
         [docs[mean].str.contains("CV") for mean in Mean_Samples], axis=1
-    )
+    ).fillna(False)
     # Mask df
     df[Mean_Samples] = df[Mean_Samples].mask(cv_fails)
     return df, df_flagged
