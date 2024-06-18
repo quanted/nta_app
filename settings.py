f8ade2e212849a1fcfd983444991256fd38f1541
JeffreyMinucci <jminucci2@gmail.com>
2024-06-18 10:35:53 -0400

    check deploy env for django debug

--- settings.py
+++ settings.py
@@ -6,13 +6,13 @@ logger = logging.getLogger(__name__)
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(message)s',
+    format="%(asctime)s %(levelname)s %(message)s",
 )
 
 logger.info("NTA-APP django:settings.py")
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-TEMPLATE_ROOT = os.path.join(PROJECT_ROOT, 'templates/')
+TEMPLATE_ROOT = os.path.join(PROJECT_ROOT, "templates/")
 
 DEPLOY_ENV = os.getenv("DEPLOY_ENV", "kube-dev")
 
@@ -26,93 +26,95 @@ LOGIN_REQUIRED = "true" == os.getenv("LOGIN_REQUIRED", "false").lower()
 LOGIN_URL = "/nta/login"
 LOGIN_VERBOSE = "true" == os.getenv("LOGIN_VERBOSE", "false").lower()
 LOGIN_DURATION = int(os.getenv("LOGIN_DURATION", 86400))
-logger.info(f"LOGIN_REQUIRED: {LOGIN_REQUIRED}, LOGIN_URL: {LOGIN_URL}, LOGIN_DURATION: {LOGIN_DURATION}, "
-            f"LOGIN_VERBOSE: {LOGIN_VERBOSE}")
+logger.info(
+    f"LOGIN_REQUIRED: {LOGIN_REQUIRED}, LOGIN_URL: {LOGIN_URL}, LOGIN_DURATION: {LOGIN_DURATION}, "
+    f"LOGIN_VERBOSE: {LOGIN_VERBOSE}"
+)
 
-if DEPLOY_ENV == "kube-dev":
+if DEPLOY_ENV == "kube-dev":  # run in dev mode
     DEBUG = True
     CORS_ORIGIN_ALLOW_ALL = True
-else:
+    logger.info("Running in Debug mode!")
+else:  # Run in Production mode
     DEBUG = False
     CORS_ORIGIN_ALLOW_ALL = False
+    logger.info("Running in Production mode!")
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ["*"]
 APPEND_SLASH = True
 
 ADMINS = (
-    ('Tom Purucker', 'purucker.tom@epa.gov'),
-    ('Jeffrey Minucci', 'minucci.jeffrey@epa.gov'),
-    ('Deron Smith', 'smith.deron@epa.gov'),
+    ("Tom Purucker", "purucker.tom@epa.gov"),
+    ("Jeffrey Minucci", "minucci.jeffrey@epa.gov"),
+    ("Deron Smith", "smith.deron@epa.gov"),
 )
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
             os.path.join(TEMPLATE_ROOT),
-            os.path.join(TEMPLATE_ROOT, 'drupal_2017'),
+            os.path.join(TEMPLATE_ROOT, "drupal_2017"),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
-        }
+        },
     }
 ]
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'nta_app',
-    'corsheaders'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "nta_app",
+    "corsheaders",
 )
 
 MIDDLEWARE = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 if LOGIN_REQUIRED:
     MIDDLEWARE += [
-        'login_middleware.RequireLoginMiddleware',
-        'login_middleware.Http403Middleware',
-        'django.contrib.messages.middleware.MessageMiddleware'
+        "login_middleware.RequireLoginMiddleware",
+        "login_middleware.Http403Middleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
     ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = "urls"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(PROJECT_ROOT, "db.sqlite3"),
     },
 }
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    "default": {
+        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+LANGUAGE_CODE = "en-us"
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = "America/New_York"
 
 USE_I18N = True
 
@@ -120,25 +122,25 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    # "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "collected_static/nta")
-STATIC_URL = '/nta/static/'
+STATIC_URL = "/nta/static/"
 
 # Define ENVIRONMENTAL VARIABLES
-os.environ.update({
-    'PROJECT_PATH': PROJECT_ROOT,
-    'SITE_SKIN': 'EPA',  # Leave empty ('') for default skin, 'EPA' for EPA skin
-    'CONTACT_URL': 'https://www.epa.gov/research/forms/contact-us-about-epa-research',
-})
+os.environ.update(
+    {
+        "PROJECT_PATH": PROJECT_ROOT,
+        "SITE_SKIN": "EPA",  # Leave empty ('') for default skin, 'EPA' for EPA skin
+        "CONTACT_URL": "https://www.epa.gov/research/forms/contact-us-about-epa-research",
+    }
+)
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = "wsgi.application"
