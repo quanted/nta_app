6558dc5a04a6f6937f79d863a23d2a1d00d3602b
Alex Chao <chao.alex@epa.gov>
2024-09-05 20:00:14 +0000

    Standardize decision tree button and page title text

--- views/ms1/output.py
+++ views/ms1/output.py
@@ -6,46 +6,44 @@ from django.template.loader import render_to_string
 from .. import links_left
 
 
-#@require_POST
-def output_page(request, model='nta', header='NTA', jobid = '00000000'):
+# @require_POST
+def output_page(request, model="nta", header="NTA", jobid="00000000"):
     header = "NTA"
     model = "ms1"
     model_output_html = "<h3> Job ID: " + jobid + "</h3> <br>"
 
-    #this is where the func to generate output html will be called
+    # this is where the func to generate output html will be called
     model_output_html += file_download_buttons(jobid)
-    model_output_html += '<div id="except_info"></div>' #if there is an error, exception info will be placed here by the js script
+    model_output_html += (
+        '<div id="except_info"></div>'  # if there is an error, exception info will be placed here by the js script
+    )
     html = output_page_html(header, model, model_output_html)
     response = HttpResponse()
     response.write(html)
-    #print(html)
+    # print(html)
     return response
 
 
 def output_page_html(header, model, tables_html):
     """Generates HTML to fill '.articles_output' div on output page"""
 
-    #epa template header
-    html = render_to_string('01epa_drupal_header.html', {
-        'SITE_SKIN': os.environ['SITE_SKIN'],
-        'TITLE': u"\u00FCbertool"
-    })
-    html += render_to_string('02epa_drupal_header_bluestripe_onesidebar.html', {})
-    html += render_to_string('epa_drupal_section_title_nta.html', {})
+    # epa template header
+    html = render_to_string(
+        "01epa_drupal_header.html", {"SITE_SKIN": os.environ["SITE_SKIN"], "TITLE": "\u00FCbertool"}
+    )
+    html += render_to_string("02epa_drupal_header_bluestripe_onesidebar.html", {})
+    html += render_to_string("epa_drupal_section_title_nta.html", {})
 
-    #main body
-    html += render_to_string('nta_main_content.html', {
-        'TITLE': header + ' Output',
-        'TEXT_PARAGRAPH': tables_html
-    })
+    # main body
+    html += render_to_string("nta_main_content.html", {"TITLE": header + " Output", "TEXT_PARAGRAPH": tables_html})
     html += links_left.ordered_list(model)
 
-    #css and scripts
-    html += render_to_string('nta_scripts_css.html', {})
-    html += render_to_string('ms1/nta_output_scripts.html', {})
+    # css and scripts
+    html += render_to_string("nta_scripts_css.html", {})
+    html += render_to_string("ms1/nta_output_scripts.html", {})
 
-    #epa template footer
-    html += render_to_string('10epa_drupal_footer.html', {})
+    # epa template footer
+    html += render_to_string("10epa_drupal_footer.html", {})
     return html
 
 
@@ -54,16 +52,14 @@ def file_download_buttons(jobid):
     <div id="download_area">
         <H3 id="section1">Download results:</H3>
         <div class="buttons">
-            <input type="button" value="Final results" onclick="window.open('/nta/ms1/results/toxpi/{jobid}')">
+            <input type="button" value="Final Results" onclick="window.open('/nta/ms1/results/toxpi/{jobid}')">
         </div>
     </div>
     <div id="decision_tree_area">
         <H3 id="section1">View plots:</H3>
         <div class="buttons">
-            <input type="button" value="Filtering logic tree" onclick="window.open('/nta/ms1/results/decision_tree/{jobid}')">
+            <input type="button" value="Filtering Decision Tree" onclick="window.open('/nta/ms1/results/decision_tree/{jobid}')">
         </div>
     </div>
     """
-    return html.format(jobid = jobid)
-
-
+    return html.format(jobid=jobid)
