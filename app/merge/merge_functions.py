2f3353c66fdaae6c825503406541ca81be5de10c
Alex Chao <chao.alex@epa.gov>
2024-11-04 19:09:36 +0000

    NTAW-607: Update merge results formatting

--- app/merge/merge_functions.py
+++ app/merge/merge_functions.py
@@ -24,10 +24,7 @@ logger = logging.getLogger("nta_app.merge")
 
 
 def process_MS2_data(ms1_data, ms2_data_list, mass_accuracy=10, rt_accuracy=0.2):
-    # NTAW-158: Adjust sheet names pulled from MS1 results
     matched_df = ms1_data if isinstance(ms1_data, pd.DataFrame) else ms1_data["chemical_results"]
-    # matched_df = ms1_data if isinstance(ms1_data, pd.DataFrame) else ms1_data["dsstox_search"]
-
     matched_df.rename(columns={"DTXCID_INDIVIDUAL_COMPONENT": "DTXCID"}, inplace=True)
 
     for ms2_data in ms2_data_list:
@@ -44,7 +41,6 @@ def process_MS2_data(ms1_data, ms2_data_list, mass_accuracy=10, rt_accuracy=0.2)
         # logger.info('mass_col, rt_col, score_col')
         # logger.info(mass_col, rt_col, score_col)
 
-        # NTAW-158: Adjust columns renamed based on new MS2 column names
         # NTAW-158: Grab the neutral mass column from the MS2 data as this is going to be compared to the neutral mass from the MS1 data
         cfmid_df.rename(
             columns={
@@ -56,16 +52,19 @@ def process_MS2_data(ms1_data, ms2_data_list, mass_accuracy=10, rt_accuracy=0.2)
             },
             inplace=True,
         )
-        # cfmid_df.rename(columns={"MASS_MGF": mass_col, "RT": rt_col, "SUM_SCORE": score_col}, inplace=True)
-        # cfmid_df.rename(columns = {'MASS_MGF': mass_col, 'RT': rt_col, 'SUM_SCORE' : score_col}, inplace = True)
-        # cfmid_df.rename(columns={"MASS_in_MGF": mass_col, "RT": rt_col, "energy_sum": score_col}, inplace=True)
+
+        # NTAW-607: Convert retention time column units from seconds to minutes
+        cfmid_df[rt_col] = cfmid_df[rt_col] / 60
+
+        # NTAW-607: Add units to MS1 retention time column
+        matched_df.rename(columns={"Retention_Time": "Retention_Time(min)"}, inplace=True)
 
         matched_df = matched_df.merge(
             cfmid_df[
                 [
                     "DTXCID",
                     f"MASS_MGF_{filename}",
-                    f"RT_{filename}",
+                    f"RT_{filename}(min)",
                     f"SUM_SCORE_{filename}",
                     f"QUOTIENT_SCORE_{filename}",
                     f"PERCENTILE_SCORE_{filename}",
@@ -77,7 +76,7 @@ def process_MS2_data(ms1_data, ms2_data_list, mass_accuracy=10, rt_accuracy=0.2)
         matched_df["mass_diff"] = abs(matched_df["Mass"] - matched_df[f"MASS_MGF_{filename}"])
         # NTAW-158: Retention time units of input MS1 are in minutes, input MS2 are in seconds, convert MS2 units to minutes by dividing by 60
         # matched_df["rt_diff"] = abs(matched_df["Retention_Time"] - matched_df[f"RT_{filename}"])
-        matched_df["rt_diff"] = abs(matched_df["Retention_Time"] - matched_df[f"RT_{filename}"] / 60)
+        matched_df["rt_diff"] = abs(matched_df["Retention_Time(min)"] - matched_df[f"RT_{filename}(min)"])
         matched_df["sum_diff"] = [
             mass_diff + rt_diff if mass_diff <= mass_accuracy and rt_diff <= rt_accuracy else np.nan
             for mass_diff, rt_diff in zip(matched_df["mass_diff"], matched_df["rt_diff"])
@@ -88,6 +87,15 @@ def process_MS2_data(ms1_data, ms2_data_list, mass_accuracy=10, rt_accuracy=0.2)
             (matched_df["mass_diff"] < mass_accuracy) & (matched_df["rt_diff"] < rt_accuracy),
             [np.nan, np.nan, np.nan, np.nan, np.nan],
         )
+
+        # NTAW-607: Round MS2 retention time, cfmid score columns to two decimal places
+        matched_df[f"RT_{filename}(min)"] = matched_df[f"RT_{filename}(min)"].round(2)
+        matched_df[score_col] = matched_df[score_col].round(2)
+        matched_df[q_score_col] = matched_df[q_score_col].round(2)
+        matched_df[percentile_col] = matched_df[percentile_col].round(2)
+
+    # NTAW-607: Round MS1 retention time column to two decimal places
+    matched_df["Retention_Time(min)"] = matched_df["Retention_Time(min)"].round(2)
 
     matched_df.drop(columns=["mass_diff", "rt_diff", "sum_diff"], inplace=True)
     matched_df["Median_MS2_Mass"] = matched_df[[col for col in matched_df.columns if "MASS_" in col]].apply(
