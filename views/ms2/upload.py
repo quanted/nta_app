411e024b2783413ff87328130bb6dca23859ee39
Troy M Ferland <54367666+tmferland@users.noreply.github.com>
2025-02-04 20:51:21 +0000

    Updating 'current_version' parameter in MS1 and MS2 workflows in accordance with pull to prod

--- views/ms2/upload.py
+++ views/ms2/upload.py
@@ -32,7 +32,7 @@ def upload_page(request, form_data=None, form_files=None):
     current_datetime = datetime.datetime.now()
 
     # manually define current version of the WebApp
-    current_version = "0.3.5"
+    current_version = "0.3.6"
 
     # define inputParameters dictionary containing all the parameters and their attributes, labels, and initial values
     inputParameters = {
