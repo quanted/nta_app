ed4ef003b5dd979a9cd4ea107014dd2e80c814f4
Troy M Ferland <54367666+tmferland@users.noreply.github.com>
2025-03-11 13:04:39 +0000

    Removing duplicate line from heatmap,py

--- app/ms1/heatmap.py
+++ app/ms1/heatmap.py
@@ -105,7 +105,7 @@ def occurrence_heatmap(parameters, data_map, blank_headers, sample_headers):
             cv_df.loc[dfCombined[y] < min_replicate_blanks_hits_percent, x] = np.nan
         else:
             cv_df.loc[dfCombined[y] < min_replicate_hits_percent, x] = np.nan
-        cv_df.loc[dfCombined[y] < min_replicate_hits_percent, x] = np.nan
+        # cv_df.loc[dfCombined[y] < min_replicate_hits_percent, x] = np.nan
         cv_df.loc[dfCombined[z] <= dfCombined["MDL"], x] = np.nan
     # Add sum of Trues for condition applied to cv dataframe
     cv_df["below count"] = (cv_df <= max_replicate_cv_value).sum(axis=1)
