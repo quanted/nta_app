db76ba922d02b74f2aa3112ef7f7b36cc24cbc6d
Safia Rizwan <Rizwan.Safia@EPA.gov>
2025-03-24 13:24:33 +0000

    NTAW-734: fixed error: trying to create positive input files list  when positive files are not uploaded to merge

--- views/merge/input.py
+++ views/merge/input.py
@@ -54,19 +54,14 @@ def input_page(request, form_data=None, form_files=None):
             input_data = {"MS1": None, "MS2_pos": [], "MS2_neg": []}
             ms1_input = request.FILES.getlist("ms1_inputs")
 
-            # NTAW-734
-            inputParameters["project_name"][1] = parameters["project_name"]
-            inputParameters["csrfmiddlewaretoken"][1] = parameters["csrfmiddlewaretoken"]
-            inputParameters["mass_accuracy_tolerance"][1] = parameters["mass_accuracy_tolerance"]
-            inputParameters["rt_tolerance"][1] = parameters["rt_tolerance"]
-            logger.warning(f"Merge inputParameters: {inputParameters}")
-
             # NTAW-158 = AC 6/10/2024: Update parser to handle xlsx files. An xlsx file will create a dictionary with all the results sheets; only grab the chemical results sheet if it is a dict
             ms1_input_temp = fileParser.run(ms1_input[0])
             if isinstance(ms1_input_temp, dict):
                 input_data["MS1"] = ms1_input_temp["Chemical Results"]
             else:
                 input_data["MS1"] = ms1_input_temp
+            logger.warning(f"MS1 file list: {ms1_input}")
+            inputParameters["ms1_inputs"][1] = [ms1_input]
 
             if bool(request.FILES.get("ms2_pos_inputs", False)) == True:
                 ms2_pos_input = request.FILES.getlist("ms2_pos_inputs")
@@ -74,6 +69,8 @@ def input_page(request, form_data=None, form_files=None):
                 input_data["MS2_pos"] = [
                     {"file_name": file.name, "file_df": fileParser.run(file)} for file in pos_input_list if file
                 ]
+                logger.warning(f"MS2 file list pos: {ms2_pos_input}")
+                inputParameters["ms2_pos_inputs"][1] = [ms2_pos_input]
 
             if bool(request.FILES.get("ms2_neg_inputs", False)) == True:
                 ms2_neg_input = request.FILES.getlist("ms2_neg_inputs")
@@ -81,10 +78,16 @@ def input_page(request, form_data=None, form_files=None):
                 input_data["MS2_neg"] = [
                     {"file_name": file.name, "file_df": fileParser.run(file)} for file in neg_input_list if file
                 ]
+                logger.warning(f"MS2 file list neg: {ms2_neg_input}")
+                inputParameters["ms2_neg_inputs"][1] = [ms2_neg_input]
 
-            logger.warning(f"MS1 file list: {ms1_input}")
-            logger.warning(f"MS2 file list neg: {ms2_pos_input}")
-            logger.warning(f"MS2 file list pos: {ms2_neg_input}")
+            # NTAW-734
+            inputParameters["project_name"][1] = parameters["project_name"]
+            inputParameters["csrfmiddlewaretoken"][1] = parameters["csrfmiddlewaretoken"]
+            inputParameters["mass_accuracy_tolerance"][1] = parameters["mass_accuracy_tolerance"]
+            inputParameters["rt_tolerance"][1] = parameters["rt_tolerance"]
+            logger.warning(f"Merge inputParameters: {inputParameters}")
+
             # run_merge_dask(inputParameters, input_data, job_id)
             run_merge_dask(parameters, input_data, job_id)
             return redirect("/nta/merge/processing/" + job_id, permanent=True)
