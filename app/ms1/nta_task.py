5a264470352de1f8ea6f6e11df345a2c502641c9
Troy M Ferland <54367666+tmferland@users.noreply.github.com>
2025-04-04 18:33:22 +0000

    Adding logger statements to troubleshoot run_seq_check()

--- app/ms1/nta_task.py
+++ app/ms1/nta_task.py
@@ -247,10 +247,12 @@ class NtaRun:
             logger.info("dfs.size(): {}".format(len(self.dfs)))
             if self.dfs[0] is not None:
                 logger.info("POS df length: {}".format(len(self.dfs[0])))
-                # logger.info("POS df columns: {}".format(self.dfs[0].columns.tolist()))
+                logger.info("POS df columns: {}".format(self.dfs[0].columns.tolist()))
+                logger.info("run_seq_pos df columns: {}".format(self.run_sequence_pos_df.columns.tolist()))
             if self.dfs[1] is not None:
                 logger.info("NEG df length: {}".format(len(self.dfs[1])))
-                # logger.info("NEG df columns: {}".format(self.dfs[1].columns.tolist()))
+                logger.info("NEG df columns: {}".format(self.dfs[1].columns.tolist()))
+                logger.info("run_seq_neg df columns: {}".format(self.run_sequence_pos_df.columns.tolist()))
         self.check_tracers()
         if self.verbose:
             logger.info("Checked tracers.")
