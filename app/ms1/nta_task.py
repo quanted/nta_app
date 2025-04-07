41992ff88b346fce41f16c143231e0c155896140
Safia Rizwan <Rizwan.Safia@EPA.gov>
2025-04-07 13:55:01 +0000

    NTAW-756: Fixed typo: deleted extra underscore

--- app/ms1/nta_task.py
+++ app/ms1/nta_task.py
@@ -1302,7 +1302,7 @@ class NtaRun:
         score_mapped_cols = [f"{col}_mapped" for col in score_cols]
 
         # Apply mappings and store in mapped columns
-        df.loc[:, authority_mapped__cols] = df[authority_cols].applymap(lambda x: authority_mapping.get(x, x))
+        df.loc[:, authority_mapped_cols] = df[authority_cols].applymap(lambda x: authority_mapping.get(x, x))
         df.loc[:, score_mapped_cols] = df[score_cols].applymap(lambda x: score_mapping.get(x, x))
 
         # Apply mapping to authority and hazard score columns
