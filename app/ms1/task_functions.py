7f62000c63591bfa6ff893cb4f9c32c44f4bc526
Safia Rizwan <Rizwan.Safia@EPA.gov>
2025-05-01 13:19:41 +0000

    NTAW-800: adding memory tracking statements

--- app/ms1/task_functions.py
+++ app/ms1/task_functions.py
@@ -1969,7 +1969,6 @@ def create_excel_book(d, chem_res=False):
             sheet = workbook.worksheets[sheet_num]
             # Freezes the top row of every sheet in the excel file.
             sheet.freeze_panes = "A2"
-            log_memory_usage("freeze_panes")
             # Format each column width to fit the longest string contained within the column
             for column in df:
                 try:
@@ -1980,7 +1979,6 @@ def create_excel_book(d, chem_res=False):
                 # NTAW-704: handle error where df[column] is recognised as a DataFrame, not a series
                 except AttributeError:
                     pass
-        log_memory_usage("column formatting")
         # Format DTXSID column hyperlinks an column width in the Chemical Results sheet
         if chem_res:
             workbook = writer.book
@@ -1992,6 +1990,67 @@ def create_excel_book(d, chem_res=False):
             log_memory_usage("chem_res Hyperlink formatting")
     excel_data = in_memory_buffer.getvalue()
     return excel_data
+
+
+# # NTAW-800 attempting create_excel-book
+# def create_excel_book(d, chem_res=False):
+#     """
+#     Function for creating excel book from python dictionary, where dict keys
+#     are sheet names and dict items (dfs) are sheet contents.
+
+#     Args:
+#         d (dictionary; key:dataframe items)
+#         chem_res (Boolean; does the submitted dictionary d contain Chemical Results info)
+#     Returns:
+#         excel_data (pd.ExcelWriter output)
+#     """
+
+#     # Create an excel sheet from the datamap and save it to MongoDB
+#     in_memory_buffer = io.BytesIO()
+#     # Get list of keys in the dictionary
+#     keys_list = list(d.keys())
+#     # Convert self.data_map dictionary into an excel workbook
+
+#     log_memory_usage("Start create_excel_book")
+#     with pd.ExcelWriter(in_memory_buffer, engine="xlsxwriter") as writer:
+#         workbook = writer.book
+#         hyperlink_format = workbook.add_format({'font_color': 'blue', 'underline': 1})
+
+#         for df_name, df in d.items():
+#             df.to_excel(writer, sheet_name=df_name, index=False)
+#             log_memory_usage("df.to_excel")
+
+#             worksheet = writer.sheets[df_name]
+
+#             # Freezes the top row of every sheet in the excel file.
+#             worksheet.freeze_panes(1, 0)
+
+#             # Format column widths to fit the largest string contained within the column
+#             sheet_num = keys_list.index(df_name)
+#             sheet = workbook.worksheets[sheet_num]
+
+#             log_memory_usage("freeze_panes")
+#             # Format each column width to fit the longest string contained within the column
+#             for column in df:
+#                 try:
+#                     column_width = max(df[column].astype(str).map(len).max(), len(column)) + 1
+#                     col_idx = df.columns.get_loc(column) + 1
+#                     col_letter = get_column_letter(col_idx)
+#                     sheet.column_dimensions[col_letter].width = column_width
+#                 # NTAW-704: handle error where df[column] is recognised as a DataFrame, not a series
+#                 except AttributeError:
+#                     pass
+#         # Format DTXSID column hyperlinks an column width in the Chemical Results sheet
+#         if chem_res:
+#             workbook = writer.book
+#             for sheet in workbook.worksheets:
+#                 for i in range(sheet.max_row):
+#                     cell = sheet.cell(row=i + 2, column=9)
+#                     cell.style = "Hyperlink"
+#                 sheet.column_dimensions["I"].width = 18
+#             log_memory_usage("chem_res Hyperlink formatting")
+#     excel_data = in_memory_buffer.getvalue()
+#     return excel_data
 
 
 def DSSTox_atom_filtering(df_in, atom_ranges):
