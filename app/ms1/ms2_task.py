743c15975a5a7e691c86f98ba1608a18ab9766ba
Troy M Ferland <54367666+tmferland@users.noreply.github.com>
2025-07-25 19:08:26 +0000

    NTAW-915: Commenting out chunks of ms2_task.py

--- app/ms1/ms2_task.py
+++ app/ms1/ms2_task.py
@@ -26,67 +26,67 @@ from ...tools.ms2.send_email import send_ms2_finished
 NO_DASK = False  # set this to True to run locally without dask (for debug purposes)
 
 # Set up logging
-logger = logging.getLogger("nta_app.ms2")
+logger = logging.getLogger("nta_app.ms1")
 
 
-def run_ms2_dask(parameters, jobid="00000000", verbose=True):
-    # log parameters
-    logger.info("Parameters:")
-    logger.info(parameters)
-    logger.info("Job ID: {}".format(jobid))
+# def run_ms2_dask(parameters, jobid="00000000", verbose=True):
+#     # log parameters
+#     logger.info("Parameters:")
+#     logger.info(parameters)
+#     logger.info("Job ID: {}".format(jobid))
 
-    in_docker = os.environ.get("IN_DOCKER") != "False"
-    mongo_address = os.environ.get("MONGO_SERVER")
-    link_address = reverse("ms2_results", kwargs={"jobid": jobid})
-    if NO_DASK:
-        run_ms2(parameters, mongo_address, jobid, results_link=link_address, verbose=verbose, in_docker=in_docker)
-        return
-    if not in_docker:
-        logger.info("Running in local development mode.")
-        logger.info("Detected OS is {}".format(os.environ.get("SYSTEM_NAME")))
-        local_cluster = LocalCluster(processes=False)
-        dask_client = Client(local_cluster)
-    else:
-        dask_scheduler = os.environ.get("DASK_SCHEDULER")
-        logger.info("Running in docker environment. Dask Scheduler: {}".format(dask_scheduler))
-        dask_client = Client(dask_scheduler)
-        logger.info("Dask URL: {dask_client.dashboard_link}")
-    # dask_input_dfs = dask_client.scatter(input_dfs)
-    logger.info("Submitting Nta ms2 Dask task")
-    task = dask_client.submit(
-        run_ms2,
-        parameters,
-        mongo_address,
-        jobid,
-        results_link=link_address,
-        verbose=verbose,
-        in_docker=in_docker,
-    )  # dask_input_dfs, mongo_address, jobid, results_link=link_address,verbose=verbose, in_docker=in_docker)
-    fire_and_forget(task)
+#     in_docker = os.environ.get("IN_DOCKER") != "False"
+#     mongo_address = os.environ.get("MONGO_SERVER")
+#     link_address = reverse("ms2_results", kwargs={"jobid": jobid})
+#     if NO_DASK:
+#         run_ms2(parameters, mongo_address, jobid, results_link=link_address, verbose=verbose, in_docker=in_docker)
+#         return
+#     if not in_docker:
+#         logger.info("Running in local development mode.")
+#         logger.info("Detected OS is {}".format(os.environ.get("SYSTEM_NAME")))
+#         local_cluster = LocalCluster(processes=False)
+#         dask_client = Client(local_cluster)
+#     else:
+#         dask_scheduler = os.environ.get("DASK_SCHEDULER")
+#         logger.info("Running in docker environment. Dask Scheduler: {}".format(dask_scheduler))
+#         dask_client = Client(dask_scheduler)
+#         logger.info("Dask URL: {dask_client.dashboard_link}")
+#     # dask_input_dfs = dask_client.scatter(input_dfs)
+#     logger.info("Submitting Nta ms2 Dask task")
+#     task = dask_client.submit(
+#         run_ms2,
+#         parameters,
+#         mongo_address,
+#         jobid,
+#         results_link=link_address,
+#         verbose=verbose,
+#         in_docker=in_docker,
+#     )  # dask_input_dfs, mongo_address, jobid, results_link=link_address,verbose=verbose, in_docker=in_docker)
+#     fire_and_forget(task)
 
-    try:
-        import time
+#     try:
+#         import time
 
-        time.sleep(5)
-        memory_profile = dask_client.profile()
-        logger.info("[Dask Memory Profile] %s", memory_profile)
-    except Exception as e:
-        logger.error("Failed to capture Dask memory profile: %s", repr(e))
+#         time.sleep(5)
+#         memory_profile = dask_client.profile()
+#         logger.info("[Dask Memory Profile] %s", memory_profile)
+#     except Exception as e:
+#         logger.error("Failed to capture Dask memory profile: %s", repr(e))
 
 
-def run_ms2(parameters, mongo_address=None, jobid="00000000", results_link="", verbose=True, in_docker=True):
-    ms2_run = MS2Run(parameters, mongo_address, jobid, results_link, verbose, in_docker=in_docker)
-    try:
-        ms2_run.execute()
-    except Exception as e:
-        trace = traceback.format_exc()
-        logger.info(trace)
-        fail_step = ms2_run.get_step()
-        ms2_run.set_status("Failed on step: " + fail_step)
-        error = repr(e)
-        ms2_run.set_except_message(error)
-        raise e
-    return True
+# def run_ms2(parameters, mongo_address=None, jobid="00000000", results_link="", verbose=True, in_docker=True):
+#     ms2_run = MS2Run(parameters, mongo_address, jobid, results_link, verbose, in_docker=in_docker)
+#     try:
+#         ms2_run.execute()
+#     except Exception as e:
+#         trace = traceback.format_exc()
+#         logger.info(trace)
+#         fail_step = ms2_run.get_step()
+#         ms2_run.set_status("Failed on step: " + fail_step)
+#         error = repr(e)
+#         ms2_run.set_except_message(error)
+#         raise e
+#     return True
 
 
 FILENAMES = {"final_output": ["CFMID_results_pos", "CFMID_results_neg", "input_parameters"]}
@@ -98,11 +98,11 @@ class MS2Run:
         parameters=None,
         mongo_address=None,
         jobid="00000000",
-        results_link=None,
-        verbose=True,
-        in_docker=True,
+        # results_link=None,
+        # verbose=True,
+        # in_docker=True,
     ):
-        logger.info("[Job ID: {}] MS2Run initialize - started".format(jobid))
+        # logger.info("[Job ID: {}] MS2Run initialize - started".format(jobid))
         self.inputParameters = parameters["inputParameters"]
         self.project_name = parameters["project_name"]
         self.n_masses = 1
@@ -111,12 +111,12 @@ class MS2Run:
         self.features = {"pos": [], "neg": []}
         self.cfmid_responses = []
         # self.email = parameters['results_email']
-        self.results_link = results_link
+        # self.results_link = results_link
         self.precursor_mass_accuracy = float(parameters["precursor_mass_accuracy"])
         self.fragment_mass_accuracy = float(parameters["fragment_mass_accuracy"])
         self.jobid = jobid
-        self.verbose = verbose
-        self.in_docker = in_docker
+        # self.verbose = verbose
+        # self.in_docker = in_docker
         self.mongo_address = mongo_address
         self.mongo = connect_to_mongoDB(self.mongo_address)
         self.gridfs = connect_to_mongo_gridfs(self.mongo_address)
@@ -387,31 +387,31 @@ class MS2Run:
         )
         pos_df = pd.merge(pos_df, self.spectra_df, on="DTXCID", how="left")
 
-        # self.mongo_save(self.features['neg'].to_df().sort_values(by = ['ID', 'Q-SCORE'], ascending = [True, False], ignore_index = True), step=FILENAMES['final_output'][0])
-        # self.mongo_save(self.features['pos'].to_df(), step=FILENAMES['final_output'][1])
-        # 2/23/2023 Reverse the filenames index, currently pointing to the wrong file
-        self.mongo_save(
-            neg_df,
-            step=FILENAMES["final_output"][1],
-        )
-        # self.mongo_save(self.features["pos"].to_df(), step=FILENAMES["final_output"][0])
-        self.mongo_save(
-            pos_df,
-            step=FILENAMES["final_output"][0],
-        )
-        self.mongo_save(inputParameters_df, step=FILENAMES["final_output"][2])
+        # # self.mongo_save(self.features['neg'].to_df().sort_values(by = ['ID', 'Q-SCORE'], ascending = [True, False], ignore_index = True), step=FILENAMES['final_output'][0])
+        # # self.mongo_save(self.features['pos'].to_df(), step=FILENAMES['final_output'][1])
+        # # 2/23/2023 Reverse the filenames index, currently pointing to the wrong file
+        # self.mongo_save(
+        #     neg_df,
+        #     step=FILENAMES["final_output"][1],
+        # )
+        # # self.mongo_save(self.features["pos"].to_df(), step=FILENAMES["final_output"][0])
+        # self.mongo_save(
+        #     pos_df,
+        #     step=FILENAMES["final_output"][0],
+        # )
+        # self.mongo_save(inputParameters_df, step=FILENAMES["final_output"][2])
 
-    def send_email(self):
-        try:
-            # link_address = reverse('ms2_results', kwargs={'jobid': self.jobid})
-            send_ms2_finished(self.email, self.results_link)
-        except Exception as e:
-            logger.critical("email error")
-            # logger.critical("Error sending email: {}".format(e.message))
-        logger.critical("email end function")
+    # def send_email(self):
+    #     try:
+    #         # link_address = reverse('ms2_results', kwargs={'jobid': self.jobid})
+    #         send_ms2_finished(self.email, self.results_link)
+    #     except Exception as e:
+    #         logger.critical("email error")
+    #         # logger.critical("Error sending email: {}".format(e.message))
+    #     logger.critical("email end function")
 
-        self.query_progress = 0
-        self.prepare_progress = 0
+    #     self.query_progress = 0
+    #     self.prepare_progress = 0
 
     def set_status(self, status, create=False):
         logger.info(
@@ -482,16 +482,16 @@ class MS2Run:
             upsert=True,
         )
 
-    def set_except_message(self, e):
-        posts = self.mongo.posts
-        time_stamp = datetime.utcnow()
-        post_id = self.jobid + "_" + "status"
-        posts.update_one({"_id": post_id}, {"$set": {"_id": post_id, "date": time_stamp, "error_info": e}}, upsert=True)
+    # def set_except_message(self, e):
+    #     posts = self.mongo.posts
+    #     time_stamp = datetime.utcnow()
+    #     post_id = self.jobid + "_" + "status"
+    #     posts.update_one({"_id": post_id}, {"$set": {"_id": post_id, "date": time_stamp, "error_info": e}}, upsert=True)
 
     def get_step(self):
         return self.step
 
-    def mongo_save(self, file, step=""):
-        to_save = file.to_json(orient="split")
-        id = self.jobid + "_" + step
-        self.gridfs.put(to_save, _id=id, encoding="utf-8", project_name=self.project_name)
+    # def mongo_save(self, file, step=""):
+    #     to_save = file.to_json(orient="split")
+    #     id = self.jobid + "_" + step
+    #     self.gridfs.put(to_save, _id=id, encoding="utf-8", project_name=self.project_name)
